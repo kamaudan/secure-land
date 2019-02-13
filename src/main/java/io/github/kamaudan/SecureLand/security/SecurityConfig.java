542c688d9803526db0b7602601f299cfb630b6a8
Dan <kamaudan@gmail.com>
2019-02-13 12:13:13 +0300

    Changed the annotation on Security config class from @Configurations to @EnableWebFluxSecurity

--- src/main/java/io/github/kamaudan/SecureLand/security/SecurityConfig.java
+++ src/main/java/io/github/kamaudan/SecureLand/security/SecurityConfig.java
@@ -1,13 +1,14 @@
 package io.github.kamaudan.SecureLand.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@Configuration
+
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
